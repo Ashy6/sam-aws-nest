56a7c0cedcfb2fb5488098c6bb44c10ab296e9cb
ashy <ashy@example.com>
2025-12-31 06:04:25 +0800

    fix: 更新API基础URL为生产环境地址

--- apps/frontend/public/config.js
+++ apps/frontend/public/config.js
@@ -1,3 +1,6 @@
 window.__APP_CONFIG__ = {
-  API_BASE_URL: "/api",
+  API_BASE_URL: 'https://diary.rowlandw3ai.shop/api/'
+//   window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+//     ? '/api'
+//     : 'https://diary.rowlandw3ai.shop/api/',
 };
